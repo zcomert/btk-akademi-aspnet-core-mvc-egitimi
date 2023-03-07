29fbf4e9b856e195b8340dfe17a976c88da6e900
zcomert <zafer.comert@hotmail.com>
2023-03-07 14:49:21 +0300

    Candidate class has been defined.

--- BtkAkademi/Models/Candidate.cs
+++ BtkAkademi/Models/Candidate.cs
@@ -2,6 +2,17 @@ namespace BtkAkademi.Models
 {
     public class Candidate
     {
-        
+        public String? Email { get; set; } = String.Empty;
+        public String? FirstName { get; set; } = String.Empty;
+        public String? LastName { get; set; } = String.Empty;
+        public String? FullName => $"{FirstName} {LastName?.ToUpper()}";
+        public int? Age { get; set; }
+        public String? SelectedCourse { get; set; } = String.Empty;
+        public DateTime ApplyAt { get; set; }
+
+        public Candidate()
+        {
+            ApplyAt = DateTime.Now;
+        }
     }
 }
