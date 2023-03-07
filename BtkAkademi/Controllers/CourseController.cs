4150bec58d857f8fe2b6026673d4e6b3fbe26384
zcomert <zafer.comert@hotmail.com>
2023-03-07 17:02:55 +0300

    ModelBinding has been done.

--- BtkAkademi/Controllers/CourseController.cs
+++ BtkAkademi/Controllers/CourseController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using BtkAkademi.Models;
 
 namespace BtkAkademi.Controllers
 {
@@ -10,6 +11,13 @@ namespace BtkAkademi.Controllers
         }
         
         public IActionResult Apply()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Apply([FromForm] Candidate model)
         {
             return View();
         }
