320fcba8cef0269102c0f733a44a1b54fc6834e3
zcomert <zafer.comert@hotmail.com>
2023-03-07 19:13:58 +0300

    Index has been updated.

--- BtkAkademi/Controllers/CourseController.cs
+++ BtkAkademi/Controllers/CourseController.cs
@@ -7,7 +7,8 @@ namespace BtkAkademi.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var model = Repository.Applications;
+            return View(model);
         }
         
         public IActionResult Apply()
