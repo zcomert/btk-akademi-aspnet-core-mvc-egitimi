f2210fea1a0ca675647d46ffbc96dabfe7292431
zcomert <zafer.comert@hotmail.com>
2023-03-07 20:18:53 +0300

    Chapter 03 has been completed.

--- BtkAkademi/Controllers/CourseController.cs
+++ BtkAkademi/Controllers/CourseController.cs
@@ -10,7 +10,7 @@ namespace BtkAkademi.Controllers
             var model = Repository.Applications;
             return View(model);
         }
-        
+
         public IActionResult Apply()
         {
             return View();
@@ -20,8 +20,17 @@ namespace BtkAkademi.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Apply([FromForm] Candidate model)
         {
-            Repository.Add(model);
-            return View("Feedback",model);
+            if(Repository.Applications.Any(c => c.Email.Equals(model.Email)))
+            {
+                ModelState.AddModelError("","There is already an application for you.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                Repository.Add(model);
+                return View("Feedback", model);
+            }
+            return View();
         }
     }
 }
--- BtkAkademi/Models/Candidate.cs
+++ BtkAkademi/Models/Candidate.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BtkAkademi.Models
 {
     public class Candidate
     {
+        [Required(ErrorMessage = "E-mail is required.")]
         public String? Email { get; set; } = String.Empty;
+        
+        [Required(ErrorMessage = "FirstName is required.")]
         public String? FirstName { get; set; } = String.Empty;
+        
+        [Required(ErrorMessage = "LastName is required.")]
         public String? LastName { get; set; } = String.Empty;
         public String? FullName => $"{FirstName} {LastName?.ToUpper()}";
         public int? Age { get; set; }
