86c010e902a9b49270dfbcfe55d997c1e7e4e2be
zcomert <zafer.comert@hotmail.com>
2023-03-08 13:06:23 +0300

    gitignore has been added.

--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using StoreApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<RepositoryContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"));
+});
 
 var app = builder.Build();
 
@@ -8,7 +16,7 @@ app.UseHttpsRedirection();
 app.UseRouting();
 
 app.MapControllerRoute(
-    name:"default",
-    pattern:"{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
