2c71efaef0361098c6a1cef5a200a126570586a3
zcomert <zafer.comert@hotmail.com>
2023-03-09 17:25:50 +0300

    Entities has been added.

---
+++ Store/Entities/Models/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace Entities.Models;
+public class Product
+{
+    public int ProductId { get; set; }
+    public String? ProductName { get; set; } = String.Empty;
+    public decimal Price { get; set; }
+}
--- Store/StoreApp/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-namespace StoreApp.Models
-{
-    public class Product
-    {
-        public int ProductId { get; set; }
-        public String? ProductName { get; set; } = String.Empty;
-        public decimal Price { get; set; }
-    }
-}
