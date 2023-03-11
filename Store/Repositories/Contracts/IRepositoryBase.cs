8b1714258adf07d42dc0f546e16d58d7da633bd9
zcomert <zafer.comert@hotmail.com>
2023-03-11 17:03:32 +0300

    ProductController has been updated.

--- Store/Repositories/Contracts/IRepositoryBase.cs
+++ Store/Repositories/Contracts/IRepositoryBase.cs
@@ -6,5 +6,6 @@ namespace Repositories.Contracts
     {
         IQueryable<T> FindAll(bool trackChanges);
         T? FindByCondition(Expression<Func<T,bool>> expression, bool trackChanges);
+        void Create(T entity);
     }
 }
--- Store/Repositories/RepositoryBase.cs
+++ Store/Repositories/RepositoryBase.cs
@@ -14,6 +14,11 @@ namespace Repositories
             _context = context;
         }
 
+        public void Create(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
         public IQueryable<T> FindAll(bool trackChanges)
         {
             return trackChanges
