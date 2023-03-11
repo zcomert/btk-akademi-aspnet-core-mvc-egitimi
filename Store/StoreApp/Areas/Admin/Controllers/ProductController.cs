73931823b3c8945ed94e0d059820bf07d4bb4e72
zcomert <zafer.comert@hotmail.com>
2023-03-11 16:30:54 +0300

    ProductController has been updated.

--- Store/StoreApp/Areas/Admin/Controllers/ProductController.cs
+++ Store/StoreApp/Areas/Admin/Controllers/ProductController.cs
@@ -1,13 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
 
 namespace StoreApp.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class ProductController : Controller
     {
+        private readonly IServiceManager _manager;
+
+        public ProductController(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var model = _manager.ProductService.GetAllProducts(false);
+            return View(model);
         }
     }
 }
