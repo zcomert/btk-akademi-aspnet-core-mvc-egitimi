d70ac43ff3be6aaec09a8a01f7684ea0575374dd
zcomert <zafer.comert@hotmail.com>
2023-03-13 09:25:47 +0300

    File operations in process.

--- Store/Entities/Dtos/ProductDto.cs
+++ Store/Entities/Dtos/ProductDto.cs
@@ -11,7 +11,8 @@ namespace Entities.Dtos
 
         [Required(ErrorMessage = "Price is required.")]
         public decimal Price { get; init; }
-
+        public String? Summary { get; init; } = String.Empty;
+        public String? ImageUrl { get; set; }
         public int? CategoryId { get; init; }  
         
     }
--- Store/Entities/Models/Product.cs
+++ Store/Entities/Models/Product.cs
@@ -6,6 +6,8 @@ public class Product
     public int ProductId { get; set; }
     public String? ProductName { get; set; } = String.Empty;
     public decimal Price { get; set; }
+    public String? Summary { get; set; } = String.Empty;
+    public String? ImageUrl { get; set; }
     public int? CategoryId { get; set; }        // Foreign Key
     public Category? Category { get; set; }     // Navigation property
 }
