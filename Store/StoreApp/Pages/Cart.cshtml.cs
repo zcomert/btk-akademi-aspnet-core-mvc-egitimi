8d1131a306413b2b0078d6a2691baef586221d35
zcomert <zafer.comert@hotmail.com>
2023-03-14 12:15:49 +0300

    RazorPages in process.

---
+++ Store/StoreApp/Pages/Cart.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StoreApp.Pages
+{
+    public class CartModel : PageModel
+    {
+        
+    }
+}
---
+++ Store/StoreApp/Pages/Demo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StoreApp.Pages
+{
+    public class DemoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
