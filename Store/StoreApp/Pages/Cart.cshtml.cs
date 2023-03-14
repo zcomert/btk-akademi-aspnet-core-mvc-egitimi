ea1e844f2c00072a3f958ebd79e78e8c64a3e33b
zcomert <zafer.comert@hotmail.com>
2023-03-14 17:24:58 +0300

    RazorPages in process.

--- Store/StoreApp/Pages/Cart.cshtml.cs
+++ Store/StoreApp/Pages/Cart.cshtml.cs
@@ -1,9 +1,44 @@
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services.Contracts;
 
 namespace StoreApp.Pages
 {
     public class CartModel : PageModel
     {
-        
+        private readonly IServiceManager _manager;
+
+        public CartModel(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        public Cart Cart { get; set; } // IoC
+        public string ReturnUrl { get; set; } = "/";
+
+        public void OnGet(string returnUrl)
+        {
+            ReturnUrl = returnUrl ?? "/";
+        }
+
+        public IActionResult OnPost(int productId, string returnUrl)
+        {
+            Product? product = _manager
+                .ProductService
+                .GetOneProduct(productId,false);
+
+            if(product is not null)
+            {
+                Cart.AddItem(product,1);
+            }
+            return Page(); // returnUrl
+        }
+
+        public IActionResult OnPostRemove(int id, string returnUrl)
+        {
+            Cart.RemoveLine(Cart.Lines.First(cl => cl.Product.ProductId.Equals(id)).Product);
+            return Page();
+        }
     }
 }
