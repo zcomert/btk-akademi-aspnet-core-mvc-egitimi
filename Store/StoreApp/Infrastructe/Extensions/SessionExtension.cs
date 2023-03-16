0e97aee5043246180608342d9d33cffd9066a633
zcomert <zafer.comert@hotmail.com>
2023-03-16 11:46:02 +0300

    Sessions in progress.

---
+++ Store/StoreApp/Infrastructe/Extensions/SessionExtension.cs
@@ -0,0 +1,25 @@
+using System.Text.Json;
+
+namespace StoreApp.Infrastructe.Extensions
+{
+    public static class SessionExtension
+    {
+        public static void SetJson(this ISession session, string key, object value)
+        {
+            session.SetString(key,JsonSerializer.Serialize(value));
+        }
+
+        public static void SetObject<T>(this ISession session, string key, T value)
+        {
+            session.SetString(key, JsonSerializer.Serialize(value));
+        }
+
+        public static T? GetJson<T>(this ISession session, string key)
+        {
+            var data = session.GetString(key);
+            return data is null 
+                ? default(T)
+                : JsonSerializer.Deserialize<T>(data);
+        }
+    }
+}
