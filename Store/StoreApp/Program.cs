a8c9c11a4b81203e293a6e4679a008c08556ec77
zcomert <zafer.comert@hotmail.com>
2023-03-16 12:34:03 +0300

    Sessions in progress.

--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -33,7 +33,7 @@ builder.Services.AddScoped<IServiceManager, ServiceManager>();
 builder.Services.AddScoped<IProductService, ProductManager>();
 builder.Services.AddScoped<ICategoryService, CategoryManager>();
 
-builder.Services.AddSingleton<Cart>();
+builder.Services.AddScoped<Cart>();
 
 builder.Services.AddAutoMapper(typeof(Program));
 
