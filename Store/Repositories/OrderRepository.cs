0f85be682479413a5dce3a9e48102e5e7a3d3a21
zcomert <zafer.comert@hotmail.com>
2023-03-16 17:53:21 +0300

    Order in process.

---
+++ Store/Repositories/OrderRepository.cs
@@ -0,0 +1,42 @@
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Repositories.Contracts;
+
+namespace Repositories
+{
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(RepositoryContext context) : base(context)
+        {
+            
+        }
+
+        public IQueryable<Order> Orders => _context.Orders
+            .Include(o => o.Lines)
+            .ThenInclude(cl => cl.Product)
+            .OrderBy(o => o.Shipped)
+            .ThenByDescending(o => o.OrderId);
+
+        public int NumberOfInProcess => 
+            _context.Orders.Count(o => o.Shipped.Equals(false));
+
+        public void Complete(int id)
+        {
+            var order = FindByCondition(o => o.OrderId.Equals(id),true);
+            if(order is null)
+                throw new Exception("Order could not found!");
+            order.Shipped = true;
+            _context.SaveChanges();
+        }
+
+        public Order? GetOneOrder(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
