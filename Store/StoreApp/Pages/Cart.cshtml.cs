a205c596c7c0f0aab0c84eb217fff65373bef932
zcomert <zafer.comert@hotmail.com>
2023-03-16 14:10:51 +0300

    Cart in process.

--- Store/StoreApp/Pages/Cart.cshtml.cs
+++ Store/StoreApp/Pages/Cart.cshtml.cs
@@ -13,16 +13,17 @@ namespace StoreApp.Pages
         public Cart Cart { get; set; } // IoC
         public string ReturnUrl { get; set; } = "/";
 
-        public CartModel(IServiceManager manager)
+        public CartModel(IServiceManager manager, Cart cartService)
         {
             _manager = manager;
+            Cart = cartService;
         }
 
 
         public void OnGet(string returnUrl)
         {
             ReturnUrl = returnUrl ?? "/";
-            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            // Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
         }
 
         public IActionResult OnPost(int productId, string returnUrl)
@@ -33,18 +34,18 @@ namespace StoreApp.Pages
 
             if(product is not null)
             {
-                Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+                // Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
                 Cart.AddItem(product,1);
-                HttpContext.Session.SetJson<Cart>("cart",Cart);
+                // HttpContext.Session.SetJson<Cart>("cart",Cart);
             }
             return Page(); // returnUrl
         }
 
         public IActionResult OnPostRemove(int id, string returnUrl)
         {
-            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            // Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
             Cart.RemoveLine(Cart.Lines.First(cl => cl.Product.ProductId.Equals(id)).Product);
-            HttpContext.Session.SetJson<Cart>("cart",Cart);
+            // HttpContext.Session.SetJson<Cart>("cart",Cart);
             return Page(); 
         }
     }
