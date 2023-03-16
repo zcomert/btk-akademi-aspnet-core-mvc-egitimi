d384c85645c7aacd99f4bb64500c986f2c89889c
zcomert <zafer.comert@hotmail.com>
2023-03-16 15:26:59 +0300

    Cart in process.

---
+++ Store/StoreApp/Infrastructe/Extensions/HttpRequestExtension.cs
@@ -0,0 +1,12 @@
+namespace StoreApp.Infrastructe.Extensions
+{
+    public static class HttpRequestExtension
+    {
+        public static string PathAndQuery(this HttpRequest request)
+        {
+            return request.QueryString.HasValue
+                ? $"{request.Path}{request.QueryString}"
+                : request.Path.ToString();
+        }
+    }
+}
