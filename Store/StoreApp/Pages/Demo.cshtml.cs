058c1061e10c4b2d8e80c378ad3544b2a576ff6e
zcomert <zafer.comert@hotmail.com>
2023-03-16 09:44:22 +0300

    Sessions in progress.

--- Store/StoreApp/Pages/Demo.cshtml.cs
+++ Store/StoreApp/Pages/Demo.cshtml.cs
@@ -5,9 +5,15 @@ namespace StoreApp.Pages
 {
     public class DemoModel : PageModel
     {
+        public String? FullName { get; set; }
         public void OnGet()
         {
             
+        }
+
+        public void OnPost([FromForm] string name)
+        {
+            FullName = name;
         }
     }
 }
