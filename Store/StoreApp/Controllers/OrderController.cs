c300b683d4c2cf453a1cb3cbf2010493dd236d78
zcomert <zafer.comert@hotmail.com>
2023-03-16 21:08:01 +0300

    OrderController in process.

--- Store/StoreApp/Controllers/OrderController.cs
+++ Store/StoreApp/Controllers/OrderController.cs
@@ -7,12 +7,36 @@ namespace StoreApp.Controllers
     public class OrderController : Controller
     {
         private readonly IServiceManager _manager;
+        private readonly Cart _cart;
 
-        public OrderController(IServiceManager manager)
+        public OrderController(IServiceManager manager, Cart cart)
         {
             _manager = manager;
+            _cart = cart;
         }
 
         public ViewResult Checkout() => View(new Order());
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Checkout([FromForm] Order order)
+        {
+            if(_cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("","Sorry, your cart is empty.");
+            }
+
+            if(ModelState.IsValid)
+            {
+                order.Lines = _cart.Lines.ToArray();
+                _manager.OrderService.SaveOrder(order);
+                _cart.Clear();
+                return RedirectToPage("/Complete", new {OrderId = order.OrderId});
+            }
+            else
+            {
+                return View();
+            }
+        }
     }
 }
