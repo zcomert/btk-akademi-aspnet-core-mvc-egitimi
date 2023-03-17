7ff8a098aaf94225d41698635ea96c08e0706785
zcomert <zafer.comert@hotmail.com>
2023-03-17 19:17:55 +0300

    ProductRepository Extension is process.

--- Store/StoreApp/Controllers/ProductController.cs
+++ Store/StoreApp/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 using Entities.Models;
+using Entities.RequestParameters;
 using Microsoft.AspNetCore.Mvc;
 using Repositories;
 using Repositories.Contracts;
@@ -15,9 +16,9 @@ namespace StoreApp.Controllers
             _manager = manager;
         }
 
-        public IActionResult Index()
+        public IActionResult Index([FromQuery] ProductRequestParameters p)
         {
-            var model =  _manager.ProductService.GetAllProducts(false);
+            var model =  _manager.ProductService.GetAllProductsWithDetails(p);
             return View(model);
         }
 
