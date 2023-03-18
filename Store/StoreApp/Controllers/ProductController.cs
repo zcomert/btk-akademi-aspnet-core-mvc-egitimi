ce0213ce62da7f3014ee843aeefad4e7b4781c78
zcomert <zafer.comert@hotmail.com>
2023-03-18 16:29:22 +0300

    Pagination in process.

--- Store/StoreApp/Controllers/ProductController.cs
+++ Store/StoreApp/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Repositories;
 using Repositories.Contracts;
 using Services.Contracts;
+using StoreApp.Models;
 
 namespace StoreApp.Controllers
 {
@@ -18,13 +19,23 @@ namespace StoreApp.Controllers
 
         public IActionResult Index(ProductRequestParameters p)
         {
-            var model =  _manager.ProductService.GetAllProductsWithDetails(p);
-            return View(model);
+            var products = _manager.ProductService.GetAllProductsWithDetails(p);
+            var pagination = new Pagination()
+            {
+                CurrenPage = p.PageNumber,
+                ItemsPerPage = p.PageSize,
+                TotalItems = _manager.ProductService.GetAllProducts(false).Count()
+            };
+            return View(new ProductListViewModel()
+            {
+                Products = products,
+                Pagination = pagination
+            });
         }
 
-        public IActionResult Get([FromRoute(Name="id")] int id)
+        public IActionResult Get([FromRoute(Name = "id")] int id)
         {
-            var model = _manager.ProductService.GetOneProduct(id,false);
+            var model = _manager.ProductService.GetOneProduct(id, false);
             return View(model);
         }
     }
