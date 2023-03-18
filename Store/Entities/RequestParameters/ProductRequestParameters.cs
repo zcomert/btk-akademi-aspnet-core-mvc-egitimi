7492b8da684e259064f4fa8f8e452f5d8fd239b4
zcomert <zafer.comert@hotmail.com>
2023-03-18 15:56:32 +0300

    Pagination in process.

--- Store/Entities/RequestParameters/ProductRequestParameters.cs
+++ Store/Entities/RequestParameters/ProductRequestParameters.cs
@@ -6,5 +6,17 @@ namespace Entities.RequestParameters
         public int MinPrice { get; set; } = 0;
         public int MaxPrice { get; set; } = int.MaxValue;
         public bool IsValidPrice => MaxPrice > MinPrice;
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+
+        public ProductRequestParameters() : this(1,6)
+        {
+            
+        }
+        public ProductRequestParameters(int pageNumber=1, int pageSize=6)
+        {
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+        }
     }
 }
