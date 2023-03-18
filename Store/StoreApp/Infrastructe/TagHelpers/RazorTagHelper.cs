d0083faf0923d8c1d454c911f81644bb721e68b9
zcomert <zafer.comert@hotmail.com>
2023-03-18 12:11:17 +0300

    TagHelpers in process.

---
+++ Store/StoreApp/Infrastructe/TagHelpers/RazorTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace StoreApp.Infrastructe.TagHelpers
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("tag-name")]
+    public class RazorTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+
+        }
+    }
+}
---
+++ Store/StoreApp/Infrastructe/TagHelpers/TableTagHelper.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace StoreApp.Infrastructe.TagHelpers
+{
+    [HtmlTargetElement("table")]
+    public class TableTagHelper : TagHelper
+    {
+        
+    }
+}
