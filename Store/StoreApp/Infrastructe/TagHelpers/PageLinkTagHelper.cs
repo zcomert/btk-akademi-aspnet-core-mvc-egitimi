09c3306c5a17c9b4ea18a11f6d05dff63099fe06
zcomert <zafer.comert@hotmail.com>
2023-03-18 16:42:29 +0300

    Pagination in process.

--- Store/StoreApp/Infrastructe/TagHelpers/PageLinkTagHelper.cs
+++ Store/StoreApp/Infrastructe/TagHelpers/PageLinkTagHelper.cs
@@ -1,5 +1,9 @@
+
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using StoreApp.Models;
 
 namespace StoreApp.Infrastructe.TagHelpers
 {
@@ -8,9 +12,21 @@ namespace StoreApp.Infrastructe.TagHelpers
     {
         private readonly IUrlHelperFactory _urlHelperFactory;
 
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext? ViewContext { get; set; }
+
+        public Pagination PageModel { get; set; }
+        public String? PageAction { get; set; }
+
         public PageLinkTagHelper(IUrlHelperFactory urlHelperFactory)
         {
             _urlHelperFactory = urlHelperFactory;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            
         }
     }
 }
