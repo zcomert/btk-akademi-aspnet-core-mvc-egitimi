9d42c7ed3955444ab733cc6cd261cc31e35e27df
zcomert <zafer.comert@hotmail.com>
2023-03-18 14:49:05 +0300

    TagHelpers in process.

--- Store/StoreApp/Infrastructe/TagHelpers/LastestProductTagHelper.cs
+++ Store/StoreApp/Infrastructe/TagHelpers/LastestProductTagHelper.cs
@@ -1,8 +1,11 @@
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using Services.Contracts;
 
 namespace StoreApp.Infrastructe.TagHelpers
 {
+    [HtmlTargetElement("div", Attributes ="products")]
     public class LastestProductTagHelper : TagHelper
     {
         private readonly IServiceManager _manager;
@@ -14,7 +17,35 @@ namespace StoreApp.Infrastructe.TagHelpers
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            base.Process(context, output);
+            TagBuilder div = new TagBuilder("div");
+            div.Attributes.Add("class","my-3");
+           
+            TagBuilder h6 = new TagBuilder("h6");
+            h6.Attributes.Add("class","lead");
+
+            TagBuilder icon = new TagBuilder("i");
+            icon.Attributes.Add("class","fa fa-box text-secondary");
+
+            h6.InnerHtml.AppendHtml(icon);
+            h6.InnerHtml.AppendHtml(" Lastest Products");
+
+            TagBuilder ul = new TagBuilder("ul");
+            var products = _manager.ProductService.GetLastestProducts(5,false);
+            foreach (Product product in products)
+            {
+                 TagBuilder li = new TagBuilder("li");
+                 
+                 TagBuilder a = new TagBuilder("a");
+                 a.Attributes.Add("href",$"/product/get/{product.ProductId}");
+                 a.InnerHtml.AppendHtml(product.ProductName);
+                 
+                 li.InnerHtml.AppendHtml(a);
+                 ul.InnerHtml.AppendHtml(li);
+            }
+
+            div.InnerHtml.AppendHtml(h6);
+            div.InnerHtml.AppendHtml(ul);
+            output.Content.AppendHtml(div);
         }
     }
 }
