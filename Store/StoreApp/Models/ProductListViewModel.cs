653065e427385194da22f6eb61a8ae1029d8198f
zcomert <zafer.comert@hotmail.com>
2023-03-18 16:07:56 +0300

    Pagination in process.

---
+++ Store/StoreApp/Models/ProductListViewModel.cs
@@ -0,0 +1,11 @@
+using Entities.Models;
+
+namespace StoreApp.Models
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; } = Enumerable.Empty<Product>();
+        public Pagination Pagination { get; set; } = new();
+        public int TotalCount => Products.Count();
+    }
+}
