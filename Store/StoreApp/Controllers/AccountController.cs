a4301f6ac739229ac8a6add820d3db19fc157431
zcomert <zafer.comert@hotmail.com>
2023-03-19 18:11:53 +0300

    Identity in process.

--- Store/StoreApp/Controllers/AccountController.cs
+++ Store/StoreApp/Controllers/AccountController.cs
@@ -1,11 +1,43 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using StoreApp.Models;
 
 namespace StoreApp.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, 
+        SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         public IActionResult Login()
         {
+            return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login([FromForm] LoginModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                IdentityUser user = await _userManager.FindByNameAsync(model.Name);
+                if(user is not null)
+                {
+                    await _signInManager.SignOutAsync();
+                    if((await _signInManager.PasswordSignInAsync(user, model.Password,false,false)).Succeeded)
+                    {
+                        return Redirect(model?.ReturnUrl ?? "/");
+                    }
+                }
+                ModelState.AddModelError("Error","Invalid username or password.");
+            }
             return View();
         }
     }
