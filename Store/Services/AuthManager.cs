20591d79a65052768b7305d162354658f82bc763
zcomert <zafer.comert@hotmail.com>
2023-03-20 12:44:29 +0300

    Identity in process.

--- Store/Services/AuthManager.cs
+++ Store/Services/AuthManager.cs
@@ -6,7 +6,6 @@ namespace Services
     public class AuthManager : IAuthService
     {
         private readonly RoleManager<IdentityRole> _roleManager;
-
         public AuthManager(RoleManager<IdentityRole> roleManager)
         {
             _roleManager = roleManager;
