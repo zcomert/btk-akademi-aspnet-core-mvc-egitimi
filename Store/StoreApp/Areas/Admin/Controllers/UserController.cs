381b9a265eb8d5b129ec87f806b1c7523507e520
zcomert <zafer.comert@hotmail.com>
2023-03-21 11:16:14 +0300

    Identity in process.

--- Store/StoreApp/Areas/Admin/Controllers/UserController.cs
+++ Store/StoreApp/Areas/Admin/Controllers/UserController.cs
@@ -47,5 +47,17 @@ namespace StoreApp.Areas.Admin.Controllers
             var user = await _manager.AuthService.GetOneUserForUpdate(id);
             return View(user);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update([FromForm] UserDtoForUpdate userDto)
+        {
+            if(ModelState.IsValid)
+            {
+                await _manager.AuthService.Update(userDto);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
