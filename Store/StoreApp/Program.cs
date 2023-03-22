5005149a6f128f0a1933d6fa72ea52e447a392a9
zcomert <zafer.comert@hotmail.com>
2023-03-22 11:12:49 +0300

    api in process.

--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -2,6 +2,7 @@ using StoreApp.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
@@ -37,6 +38,8 @@ app.UseEndpoints(endpoints =>
     endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
     endpoints.MapRazorPages();
+
+    endpoints.MapControllers();
 });
 
 
